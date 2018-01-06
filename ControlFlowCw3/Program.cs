8dee2a1de68a77452fb9e2aedff77c31f586e1e9
PowerMtest <powermtest@gmail.com>
2018-01-06 20:15:14 +0100

    koniec na dzisiaj

--- ControlFlowCw3/Program.cs
+++ ControlFlowCw3/Program.cs
@@ -10,6 +10,29 @@ namespace ControlFlowCw3
     {
         static void Main(string[] args)
         {
+            //zapytac o liczbe
+            //z wprowadzonej liczby obliczyc silnie
+            //wyswietlic silnie z liczby (np. 5! = 120)
+
+            var liczba = 0;
+            long silnia = 1;
+
+            Console.WriteLine("Wprowadz mala liczbe, policzymy dla niej silnie");
+            liczba = Convert.ToInt32(Console.ReadLine());
+            var a = liczba;
+            while (a > 0)
+            {
+                //var b = a;
+                silnia = silnia * a ;
+                //Console.WriteLine("{0} * {1} = {2}", b, b-1, b*(b-1));
+                a--;
+                
+                continue;
+
+            }
+
+            Console.WriteLine("{0}! = {1}", liczba, silnia);
+            
         }
     }
 }
