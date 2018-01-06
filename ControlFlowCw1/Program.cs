5dadf2077eac11900dd66734ea42e3cb02c1f26f
PowerMtest <powermtest@gmail.com>
2018-01-06 19:46:50 +0100

    w trakcie cwiczen

---
+++ ControlFlowCw1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowCw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ile liczb miedzy 1 a 100 jest podzielnych przez 3
+            var counter = 0;
+            var i = 0;
+            var b = 1;
+            //var c = b % 3;
+            
+            while (i <= 100)
+            {
+                if (b % 3 == 0)
+                {
+                    
+                    counter++;
+                }
+                b++;
+                i++;
+            }
+
+            Console.WriteLine(counter);
+            //Console.WriteLine(5 % 2);
+        }
+    }
+}
---
+++ ControlFlowCw2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowCw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user to enter a number or "ok" to exit
+            //calculate sum of all previously entered numbers and display it
+            var input = 0;
+            var suma = 0;
+            var sprawdzenie = "";
+            Console.WriteLine("Enter a number or type \"OK\" to exit");
+            //if (sprawdzenie == "OK")
+            //{
+            //    Console.WriteLine("Koniec programu.");
+            //}
+
+            //if (string.IsNullOrEmpty(sprawdzenie))
+            //{
+            //    Console.WriteLine("Koniec programu.");
+            //}
+            sprawdzenie = Console.ReadLine();
+            while (sprawdzenie != "OK" && !string.IsNullOrWhiteSpace(sprawdzenie))
+            {
+                //input = Convert.ToInt32(sprawdzenie);
+                if (sprawdzenie == "OK")
+                {
+                    //suma =+ input;
+                    break;
+                }
+                input = Convert.ToInt32(sprawdzenie);
+                suma += input;
+                Console.WriteLine("Enter another number or type \"OK\" to exit");
+                sprawdzenie = Console.ReadLine();
+                continue;
+            }
+
+            //input = Convert.ToInt32(sprawdzenie);
+            //while (input.ToString() != "OK")
+            //{
+            //    //Console.WriteLine("Enter another number or type \"OK\" to exit");
+                
+            //    if (sprawdzenie == "OK")
+            //    {
+            //        //suma =+ input;
+            //        break;
+            //    }
+            //    input = Convert.ToInt32(sprawdzenie);
+            //    suma += input;
+
+            //}
+            Console.WriteLine("Suma wprowadzonych liczb to: {0}", suma);
+        }
+    }
+}
