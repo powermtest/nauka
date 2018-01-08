542c7269c10ba2b62dccfb410bdc17216c17ab5c
PowerMtest <powermtest@gmail.com>
2018-01-08 21:45:15 +0100

    zadania skonczone

--- ControlFlowCw5/Program.cs
+++ ControlFlowCw5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,15 +18,34 @@ namespace ControlFlowCw5
             var userInput = string.Empty;
             Console.WriteLine("Wprowadz pare liczb po przecinku, a sprawdzimy ktora z nich jest najwieksza.");
             userInput = Console.ReadLine();
-            var poprawionyUserInput = userInput.Remove(userInput.IndexOf(", "),2);
+            var stringTylkoLiczby = new StringBuilder();
+            //var numeryczneTylkoLiczby = new int[stringTylkoLiczby.Length];
+            //var tablicaZnakow = new char[stringTylkoLiczby.Length];
+
+            //var poprawionyUserInput = userInput.Remove(userInput.IndexOf(", "),2);
             //var check = userInput.IndexOf(", ");
 
             //zrobic to loopem foreach character in string
             //spawdzic czy jest cyfra
             //jezeli tak, to dodac do nowego stringa
             //co to jest string builder?
+            foreach (var cyferka in userInput)
+            {
+                
+                if (cyferka >= '0' && cyferka <= '9')
+                {
+                    stringTylkoLiczby.Append(cyferka);
+                }
 
+            }
 
+            //stringTylkoLiczby.ToString().Max();
+            
+            //Console.WriteLine("Teraz mamy wszystkie cyferki jako String: {0}.",stringTylkoLiczby);
+            //Console.WriteLine("Trzeba je zmienic na cos z czego mozna wyciagnac max.");
+
+            Console.WriteLine("najwieksza wprowadzona liczba to {0}", stringTylkoLiczby.ToString().Max());
+            
         }
     }
 }
