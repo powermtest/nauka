348236926cfde192f502dbb1e13a4573fb3c8ed1
PowerMtest <powermtest@gmail.com>
2018-01-08 05:35:42 +0100

    kolejne cwiczenia

---
+++ ControlFlowCw4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowCw4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 - Write a program that picks a random number between 1 and 10.
+            //Give the user 4 chances to guess the number.
+            //If the user guesses the number, display “You won"; otherwise, display “You lost". 
+            //(To make sure the program is behaving correctly, you can display the secret number on the console first.)
+
+            var zmiennaLosowa = new Random().Next(1,10);
+            var userInput = String.Empty;
+            var iloscProb = 0;
+            //zmiennaLosowa.Next(1, 10);
+            //Console.WriteLine("wylosowano {0}", zmiennaLosowa); //sprawdzanie co zostalo wylosowane
+            Console.WriteLine("Wylosowano numer w przedziale od 1 do 10. \nZganij jaki to numer. Masz 4 szanse.");
+            userInput = Console.ReadLine();
+            
+
+            while (iloscProb < 4)
+            {
+                //var wprowadzonaLiczba = Convert.ToByte(userInput);
+                if (userInput == zmiennaLosowa.ToString())
+                {
+                    Console.WriteLine("Gratulacje! Udalo co sie odgadnac liczbe za {0} razem!", iloscProb+1);
+                    break;
+                }
+
+                iloscProb++;
+                //Console.WriteLine("Sprobuj jeszcze raz. --wylosowana liczba to: {0}",zmiennaLosowa);
+               
+                if (iloscProb == 4)
+                {
+                    Console.WriteLine("Wylosowana liczba to {0} - nie udalo sie...", zmiennaLosowa);
+                    break;
+                }
+                Console.WriteLine("Zostaly jeszcze {0} proby.\nSprobuj jeszcze raz.",4-iloscProb);
+                
+                userInput = Console.ReadLine();
+                continue;
+            }
+
+            
+            
+
+        }
+    }
+}
---
+++ ControlFlowCw5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowCw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5- Write a program and ask the user to enter a series of numbers separated by comma. 
+            //Find the maximum of the numbers and display it on the console. 
+            //For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+
+            var userInput = string.Empty;
+            Console.WriteLine("Wprowadz pare liczb po przecinku, a sprawdzimy ktora z nich jest najwieksza.");
+            userInput = Console.ReadLine();
+            var poprawionyUserInput = userInput.Remove(userInput.IndexOf(", "),2);
+            //var check = userInput.IndexOf(", ");
+
+            //zrobic to loopem foreach character in string
+            //spawdzic czy jest cyfra
+            //jezeli tak, to dodac do nowego stringa
+            //co to jest string builder?
+
+
+        }
+    }
+}
